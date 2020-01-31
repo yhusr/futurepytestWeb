32391c16ba8a055c0dfbf4856a3097fccc82ffa7
yhusr <904239064@qq.com>
2020-01-31 11:23:53 +0800

    新的作业

--- TestCases/test_04_private_letter.py
+++ TestCases/test_04_private_letter.py
@@ -12,6 +12,6 @@ from PageObjects.private_letter_page import PrivateLetter as PL
 @pytest.mark.usefixtures('letter')
 class TestPrivateLetter:
     # 进入私信并发送内容
-    def test_sent_private(self, home_fre):
-        PL(home_fre).go_into_private_letter(content=PD.success_data['letter_message'])
-        assert PL(home_fre).if_sent_text(sent_message=PD.success_data['letter_message'])
+    def test_sent_private(self, letter):
+        PL(letter).go_into_private_letter(content=PD.success_data['letter_message'])
+        assert PL(letter).if_sent_text(sent_message=PD.success_data['letter_message'])
